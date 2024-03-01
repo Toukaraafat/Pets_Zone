a01ec2cbd2c431d80c6373b3b909b69d2139fdd2
Touka Raafat <107259388+Toukaraafat@users.noreply.github.com>
2024-03-01 17:06:03 +0200

    commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { SliderComponent } from '../slider/slider.component';
 import { AboutComponent } from '../about/about.component';
-
+import { BlogSectionComponent } from '../blog-section/blog-section.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SliderComponent,AboutComponent],
+  imports: [SliderComponent,AboutComponent,BlogSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
