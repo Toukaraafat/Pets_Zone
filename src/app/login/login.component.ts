e6a75a411276a8ad4f0f12e74feffea3ada21021
toqa hatem <toqahatem802@gmail.com>
2024-03-02 22:03:00 +0200

    edit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink ,RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
