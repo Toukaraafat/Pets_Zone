547e34b16d97f28fd4fd02dfe867e47ab7785892
toqa hatem <toqahatem802@gmail.com>
2024-03-02 02:45:08 +0200

    edit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { SliderComponent } from '../slider/slider.component';
 import { AboutComponent } from '../about/about.component';
 import { BlogSectionComponent } from '../blog-section/blog-section.component';
+import { RegisterComponent} from '../register/register.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SliderComponent,AboutComponent,BlogSectionComponent],
+  imports: [SliderComponent,AboutComponent,BlogSectionComponent,RegisterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
