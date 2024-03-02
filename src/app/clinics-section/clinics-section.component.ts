af904c1817a8c1b3253f1aff25c29f842f07532a
Touka Raafat <107259388+Toukaraafat@users.noreply.github.com>
2024-03-02 10:42:22 +0200

    commit

---
+++ src/app/clinics-section/clinics-section.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+@Component({
+  selector: 'app-clinics-section',
+  standalone: true,
+  imports: [CarouselModule ],
+  templateUrl: './clinics-section.component.html',
+  styleUrl: './clinics-section.component.css'
+})
+export class ClinicsSectionComponent {
+  customOptions: OwlOptions = {
+    loop: true,
+    autoplay: true,
+    dots: true,
+    margin: 15,
+    autoHeight: true,
+    autoWidth: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      }
+    }
+  }
+
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,10 +3,11 @@ import { SliderComponent } from '../slider/slider.component';
 import { AboutComponent } from '../about/about.component';
 import { BlogSectionComponent } from '../blog-section/blog-section.component';
 import { PetsSectionComponent } from '../pets-section/pets-section.component';
+import { ClinicsSectionComponent } from '../clinics-section/clinics-section.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SliderComponent,AboutComponent,BlogSectionComponent, PetsSectionComponent],
+  imports: [SliderComponent,AboutComponent,BlogSectionComponent, PetsSectionComponent,ClinicsSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
