4ec79cbeafc689939f70846434f6a8ee777a7e68
toqa hatem <toqahatem802@gmail.com>
2024-03-02 04:18:41 +0200

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SliderComponent } from './slider/slider.component';
+import { RegisterComponent} from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import{TopHeaderComponent} from './top-header/top-header.component';
 import AOS from "aos";
 AOS.init();
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , HeaderComponent , FooterComponent,SliderComponent],
+  imports: [RouterOutlet , HeaderComponent , FooterComponent,SliderComponent,
+    RegisterComponent,LoginComponent,TopHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
