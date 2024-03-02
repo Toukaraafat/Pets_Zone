e523daa14e5c4e8b124d0e26ec791d24c4f1246c
Touka Raafat <107259388+Toukaraafat@users.noreply.github.com>
2024-03-02 22:29:43 +0200

    commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink ,RouterLinkActive} from '@angular/router';
+import { InnerHeaderComponent } from '../inner-header/inner-header.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive],
+  imports: [RouterLink,RouterLinkActive ,InnerHeaderComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
