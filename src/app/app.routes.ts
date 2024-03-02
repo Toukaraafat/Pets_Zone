b1dec17eaecbe252510cf7c0296f6e0d8eeb2796
Touka Raafat <107259388+Toukaraafat@users.noreply.github.com>
2024-03-02 13:37:06 +0200

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,10 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { BlogsPageComponent } from './blogs-page/blogs-page.component';
+import { AccountComponent } from './account/account.component';
+import { ShopComponent } from './shop/shop.component';
+import { CartComponent } from './cart/cart.component';
 
 export const routes: Routes = [
   {
@@ -11,13 +15,31 @@ export const routes: Routes = [
     component: HomeComponent,
     title: 'Home',
   },
-  { path: 'register', component: RegisterComponent, title: 'register' },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    title: 'register',
+  },
   {
     path: 'login',
     component: LoginComponent,
     title: 'Login',
   },
-
+  {
+    path: 'shop',
+    component: LoginComponent,
+    title: 'Shop',
+  },
+  {
+    path: 'account',
+    component: LoginComponent,
+    title: 'Account',
+  },
+  {
+    path: 'cart',
+    component: LoginComponent,
+    title: 'Cart',
+  },
   {
     path: '**',
     component: NotFoundComponent,
---
+++ src/app/blogs-page/blogs-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-blogs-page',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './blogs-page.component.html',
+  styleUrl: './blogs-page.component.css'
+})
+export class BlogsPageComponent {
+
+}
