f09cbe56ddb46df83d3f8f0aa776fb9c1f847dc5
Touka Raafat <107259388+Toukaraafat@users.noreply.github.com>
2024-03-03 23:02:44 +0200

    commit

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -4,16 +4,13 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { RouterLink ,RouterLinkActive} from '@angular/router';
 import { BlogCardComponent } from '../blog-card/blog-card.component';
-import { BlogSectionComponent } from "../blog-section/blog-section.component";
-import { PetsSectionComponent } from "../pets-section/pets-section.component";
 import { PetCardComponent} from "../pet-card/pet-card.component";
 @Component({
     selector: 'app-account',
     standalone: true,
     templateUrl: './account.component.html',
     styleUrl: './account.component.css',
-    imports: [RouterLink, RouterLinkActive, BlogCardComponent, BlogSectionComponent ,
-      PetsSectionComponent,PetCardComponent,CarouselModule]
+    imports: [RouterLink, RouterLinkActive, BlogCardComponent,PetCardComponent,CarouselModule]
 })
 export class AccountComponent {
 customOptions: OwlOptions = {
