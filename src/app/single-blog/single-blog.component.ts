858e274d94baa5e9cc77465a3c7e77e1a7bedf44
Touka Raafat <107259388+Toukaraafat@users.noreply.github.com>
2024-03-09 19:02:52 +0200

    commit

--- src/app/single-blog/single-blog.component.ts
+++ src/app/single-blog/single-blog.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, Input } from '@angular/core';
+import { Router, RouterLink ,RouterLinkActive} from '@angular/router';
 import { BlogCardComponent } from '../blog-card/blog-card.component';
+import { PostsService } from '../services/posts.service';
+import { Post } from '../interface/post';
 
 @Component({
   selector: 'app-single-blog',
   standalone: true,
-  imports: [RouterLink ,BlogCardComponent,RouterLinkActive,ShowComponent],
+  imports: [RouterLink ,BlogCardComponent,RouterLinkActive],
   templateUrl: './single-blog.component.html',
   styleUrl: './single-blog.component.css'
 })
